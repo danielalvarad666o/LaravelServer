b793a51bcdeaa96f0ed797ef6e8bb09d4193e43a
IzekeJalter <108090104+IzekeJalter@users.noreply.github.com>
2022-12-10 16:06:00 -0600

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-=======
             $table->string('nombre', 20);
             $table->string('apellidos', 30);
             $table->unsignedBigInteger('edad');
@@ -32,7 +26,6 @@ return new class extends Migration
             $table->unsignedBigInteger('numero_tarjeta');
             $table->foreign('numero_tarjeta')->references('id')->on('tarjetas');
             $table->boolean('status')->default(0);
->>>>>>> 9e6cb941063e61dcf88de33546dfce8f91efd2d0
             $table->rememberToken();
             $table->timestamps();
         });
