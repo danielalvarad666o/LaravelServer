45fd36f064de61c1c9ae7ee4c5a36365a4e68fb1
IzekeJalter <108090104+IzekeJalter@users.noreply.github.com>
2022-12-10 16:06:54 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,9 @@
 <?php
 
-<<<<<<< HEAD
-=======
+
 use App\Http\Controllers\Parque\ParqueController;
 use App\Http\Controllers\Parque\SensorController;
 use App\Http\Controllers\Parque\UsuarioController;
->>>>>>> 9e6cb941063e61dcf88de33546dfce8f91efd2d0
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,10 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
-=======
 
 Route::post("/registroDueño", [UsuarioController::class, "crearDueño"]);
 Route::get("/pero", [SensorController::class, "getAllSensores"]);
 Route::post("/addParque", [ParqueController::class, "addParque"]);
->>>>>>> 9e6cb941063e61dcf88de33546dfce8f91efd2d0
